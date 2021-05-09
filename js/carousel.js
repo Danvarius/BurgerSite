ffebb241c15aeeb01633334065cd87168d87e9f1
Ivan <durbala96@gmail.com>
2021-05-09 21:35:35 +0300

    Updates

--- js/carousel.js
+++ js/carousel.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
    $('.customer__box').slick({
       dots: false,
       arrows: true,
-      prevArrow: "<img src='../../img/left-arrow.svg' width='20' height='20' class='prev' alt='1'>",
-      nextArrow: "<img src='../../img/right-arrow.svg' width='20' height='20' class='next' alt='2'>",
+      prevArrow: "<img src='./img/left-arrow.svg' width='20' height='20' class='prev' alt='1'>",
+      nextArrow: "<img src='./img/right-arrow.svg' width='20' height='20' class='next' alt='2'>",
       autoplay: true,
       autoplaySpeed: 4000,
       adaptiveHeight: true,
